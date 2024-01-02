d414ced5aa3d5ab9556e99e69b7a8ae1ff6c0fc8
yuyuhi <zaien24@naver.com>
2024-01-03 01:01:33 +0900

    테스트코드 작성 - 05.회원가입과 로그인 테스트

---
+++ src/main/java/com/fastcampus/sns/service/UserService.java
@@ -0,0 +1,42 @@
+package com.fastcampus.sns.service;
+
+import com.fastcampus.sns.exception.SnsApplicationException;
+import com.fastcampus.sns.model.User;
+import com.fastcampus.sns.model.entity.UserEntity;
+import com.fastcampus.sns.repository.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserEntityRepository userEntityRepository;
+
+    // TODO : implement
+    public User join(String userName, String password) {
+        // 회원가입하려는 userName으로 회원가입된 user가 있는지
+        Optional<UserEntity> userEntity = userEntityRepository.findByUserName(userName);
+
+        // 회원가입 진행 = user를 등록
+        userEntityRepository.save(new UserEntity());
+
+        return new User();
+
+    }
+
+    // TODO : implement
+    public String login(String userName, String password) {
+        // 회원가입 여부 체크
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException());
+
+        // 비밀번호 체크
+        if (!userEntity.getPassword().equals(password)) {
+            throw new SnsApplicationException();
+        }
+        return "";
+    }
+}
